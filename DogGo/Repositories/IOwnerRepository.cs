8ca3c0cbde072afc2b9af76504d2875dcefb2e2c
Ali Paradise <alison.paradise@gmail.com>
2022-03-29 14:20:29 -0500

    Adding and Updating data with MVC

--- DogGo/Repositories/IOwnerRepository.cs
+++ DogGo/Repositories/IOwnerRepository.cs
@@ -8,5 +8,9 @@ namespace DogGo.Repositories
     {
         List<Owner> GetAllOwners();
         Owner GetOwnerById(int id);
+        Owner GetOwnerByEmail(string name);
+        public void AddOwner(Owner owner);   
+        public void UpdateOwner(Owner owner);
+        public void DeleteOwner(int id);
     }
 }
